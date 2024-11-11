d91baf949a3579ffb7308c9ba32f893a125b9e0b
taissafelisberto <144146064+taissafelisberto@users.noreply.github.com>
2024-11-11 11:49:52 -0300

    Add files via upload

---
+++ flashcard-aula_4/assets/perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript',
+    'O JavaScript é uma linguagem de Proramação',
+)
+
+criacartao(
+    'Geografia'
+    'Qual a Capital da França?'
+    'A Capital da França é Paris'
+
+)
+criaCartao(
+    'Programação'
+    'O que é uma função?'
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
